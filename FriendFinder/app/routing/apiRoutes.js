67afebd6513dcd919385aa26135d90ef1564c4d4
jpalacpac <jpalacpac07@gmail.com>
2017-07-22 21:46:26 -0700

    Adding equation to match buddies

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -9,9 +9,33 @@ module.exports = function(app) {
 	}); 
 
 	app.post("/api/friends", function(req, res) {
-		var addFriend = req.body;
-		userData.push(addFriend);
-		res.json(userData[0]);
-	});
+	
+		var buddyMatch = {
+			name: "",
+			photo: "",
+			friendDiff: 1000
+		}
 
+		var userData = req.body;
+		var buddyScore = userData.scores;
+		var totalDifference = 0;
+
+		for (var i=0; i < friends.length; i++){
+			// console.log(friends[i].name);
+			totalDifference = 0;
+
+			for (var j=0; j < friends[i].scores[j]; j++){
+				totalDifference += Math.abs(parseInt(buddyScore[j]) - parseInt(friends[i].scores[j]));
+
+				if (totalDifference <= buddyMatch.friendDiff){
+					buddyMatch.name = friends[i].name;
+					buddyMatch.photo = friends[i].photo;
+					buddyMatch.friendDiff = totalDifference;
+				}
+			}
+		}
+
+		friends.push(userData);
+		res.json(buddyMatch);				
+	});
 } 
